3afeec155d5004b77903711d2da90af74b17d65d
abdullahkhan44 <86144086+abdullahkhan44@users.noreply.github.com>
2022-11-25 08:08:10 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import NewFile from "./NewFile";
+
+function App() {
+  return (
+    <div className="App">
+      <NewFile name="Abdullah" age={29} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NewFile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewFile = ({ name, age }) => {
+  return (
+    <h1>
+      Hello world from {name} my age is {age - 10}
+    </h1>
+  );
+};
+
+export default NewFile;
